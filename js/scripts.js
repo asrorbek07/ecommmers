3b6085e0309da5255b2ae14fe43a6055d85a2da9
asrorbek07 <asrorobidjonov2000@gmail.com>
2021-05-28 11:31:28 +0500

    site header responsive finished

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let header = document.querySelector('.site-header'),
+    togglerBtn = document.querySelector('.header-main__menu-toggler');
+
+if(togglerBtn){
+    togglerBtn.addEventListener('click', () => {
+        header.classList.toggle('site-header__open')
+    })
+}
