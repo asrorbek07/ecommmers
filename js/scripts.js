d143ae21402dbeace6655a29c219419a74f4324a
asrorbek07 <asrorobidjonov2000@gmail.com>
2021-06-11 15:53:38 +0500

    dish page finished

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,17 @@ if(togglerBtn){
         header.classList.toggle('site-header__open')
     })
 }
+
+document.querySelector('.grid').addEventListener('click',()=>{
+    document.querySelector('.fruits__grid-view').classList.remove('active')
+    document.querySelector('.list-view').classList.add('active')
+    document.querySelector('.grid').classList.add('list-active')
+    document.querySelector('.list').classList.remove('list-active')
+})
+
+document.querySelector('.list').addEventListener('click',()=>{
+    document.querySelector('.fruits__grid-view').classList.add('active')
+    document.querySelector('.list-view').classList.remove('active')
+    document.querySelector('.grid').classList.remove('list-active')
+    document.querySelector('.list').classList.add('list-active')
+})
